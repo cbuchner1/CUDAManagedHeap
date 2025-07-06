8b36b3292a7d384b936499823ebff30dadbaea64
Christian Buchner <Christian.Buchner@gmail.com>
2025-07-07 01:21:04 +0200

    Use a more elegant way of explicitly instantiating heapAllocKernel and heapDeallocKernel for the example code

--- examples/cudamanagedheap.cuh
+++ examples/cudamanagedheap.cuh
@@ -71,9 +71,5 @@ __device__ void  free(void* p) __THROW
 #endif //__CUDACC__
 
 // explicitly force instantiation of these kernels.
-// for whatever reason this is not happening automatically.
-static void dummy()
-{
-  GPUTools::heapAllocKernel<<<1,1>>>(theHeap_ph, 123, nullptr);
-  GPUTools::heapDeallocKernel<<<1,1>>>(theHeap_ph, nullptr);
-}
+template __global__ void GPUTools::heapAllocKernel(heap_t* heap, size_t bytes, void** result);
+template __global__ void GPUTools::heapDeallocKernel(heap_t* heap, void* ptr);
